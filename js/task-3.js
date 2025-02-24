ee0892078587c3ac553d426e456a34ed85dcb31a
AlonKo68 <151028634+AlonKo68@users.noreply.github.com>
2025-02-24 11:38:34 +0100

    create

--- js/task-3.js
+++
@@ -1,8 +0,0 @@
-
-const nameInput = document.querySelector('#name-input');
-const nameOutput = document.querySelector('#name-output');
-
-nameInput.addEventListener('input', () => {
-    const nameInputValue = nameInput.value.trim();
-    nameOutput.textContent = nameInputValue === "" ? 'Anonymous' : nameInputValue;
-});
